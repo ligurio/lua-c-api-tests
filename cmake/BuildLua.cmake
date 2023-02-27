5868a5f85073446ae0c2b6f93576758b6670f4e8
Sergey Bronnikov <estetus@gmail.com>
2023-02-27 21:07:57 +0300

    tests: initial commit
    Patch adds an infrastructure for building Lua library using LuaJIT
    source code and PUC Rio Lua source code and a single test for
    luaL_addgsub() function.

---
+++ cmake/BuildLua.cmake
@@ -0,0 +1,41 @@
+macro(build_lua LUA_VERSION)
+    set(LUA_SOURCE_DIR ${PROJECT_BINARY_DIR}/lua-${LUA_VERSION}/source)
+    set(LUA_BINARY_DIR ${PROJECT_BINARY_DIR}/lua-${LUA_VERSION}/work)
+
+    set(LUA_PATCH_PATH ${PROJECT_SOURCE_DIR}/patches/puc-rio-lua.patch)
+
+    set(CFLAGS "-fsanitize=fuzzer-no-link")
+    set(LDFLAGS "-fsanitize=fuzzer-no-link")
+
+    include(ExternalProject)
+
+    ExternalProject_Add(patched-lua-${LUA_VERSION}
+        GIT_REPOSITORY https://github.com/lua/lua
+        GIT_TAG ${LUA_VERSION}
+        GIT_PROGRESS TRUE
+        GIT_SHALLOW TRUE
+
+        SOURCE_DIR ${LUA_SOURCE_DIR}
+        BINARY_DIR ${LUA_BINARY_DIR}
+        DOWNLOAD_DIR ${LUA_BINARY_DIR}
+        TMP_DIR ${LUA_BINARY_DIR}/tmp
+        STAMP_DIR ${LUA_BINARY_DIR}/stamp
+
+        PATCH_COMMAND cd <SOURCE_DIR> && patch -p1 -i ${LUA_PATCH_PATH}
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND cd <SOURCE_DIR> && make -j CC=${CMAKE_C_COMPILER} CFLAGS=${CFLAGS} LDFLAGS=${LDFLAGS}
+        INSTALL_COMMAND ""
+        UPDATE_DISCONNECTED ON
+
+        BUILD_BYPRODUCTS ${PROJECT_BINARY_DIR}/lua-${LUA_VERSION}/source/liblua.a
+    )
+
+    set(LUA_INCLUDE_DIR ${PROJECT_BINARY_DIR}/lua-${LUA_VERSION}/source/)
+    set(LUA_LIBRARIES ${PROJECT_BINARY_DIR}/lua-${LUA_VERSION}/source/liblua.a)
+    set(LUA_VERSION_STRING "PUC Rio Lua ${LUA_VERSION}")
+    set(LUA_TARGET patched-lua-${LUA_VERSION})
+
+    unset(LUA_SOURCE_DIR)
+    unset(LUA_BINARY_DIR)
+    unset(LUA_PATCH_PATH)
+endmacro(build_lua)
---
+++ cmake/BuildLuaJIT.cmake
@@ -0,0 +1,38 @@
+macro(build_luajit LJ_VERSION)
+    set(LJ_SOURCE_DIR ${PROJECT_BINARY_DIR}/luajit-${LJ_VERSION}/source)
+    set(LJ_BINARY_DIR ${PROJECT_BINARY_DIR}/luajit-${LJ_VERSION}/work)
+
+    set(CFLAGS "-fsanitize=fuzzer-no-link")
+    set(LDFLAGS "-fsanitize=fuzzer-no-link")
+
+    include(ExternalProject)
+
+    ExternalProject_Add(patched-luajit-${LJ_VERSION}
+        GIT_REPOSITORY https://github.com/LuaJIT/LuaJIT
+        GIT_TAG ${LJ_VERSION}
+        GIT_PROGRESS TRUE
+        GIT_SHALLOW TRUE
+
+        SOURCE_DIR ${LJ_SOURCE_DIR}
+        BINARY_DIR ${LJ_BINARY_DIR}/luajit-${LJ_VERSION}
+        DOWNLOAD_DIR ${LJ_BINARY_DIR}
+        TMP_DIR ${LJ_BINARY_DIR}/tmp
+        STAMP_DIR ${LJ_BINARY_DIR}/stamp
+        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND cd <SOURCE_DIR> && make -j CC=${CMAKE_C_COMPILER} CFLAGS=${CFLAGS} LDFLAGS=${LDFLAGS}
+        INSTALL_COMMAND ""
+        UPDATE_DISCONNECTED ON
+
+        BUILD_BYPRODUCTS ${LJ_SOURCE_DIR}/src/libluajit.a
+    )
+
+    set(LUA_INCLUDE_DIR ${LJ_SOURCE_DIR}/src/)
+    set(LUA_LIBRARIES ${LJ_SOURCE_DIR}/src/libluajit.a)
+    set(LUA_VERSION_STRING "LuaJIT ${LJ_VERSION}")
+    set(LUA_TARGET patched-luajit-${LJ_VERSION})
+
+    unset(LJ_SOURCE_DIR)
+    unset(LJ_BINARY_DIR)
+endmacro(build_luajit)

9d533d26a734dcb78b725b05adff8031aa7a7d41
Sergey Bronnikov <estetus@gmail.com>
2023-02-28 18:04:37 +0300

    tests: add luaL_traceback_test

---
+++ tests/luaL_traceback_test.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	lua_State *L = luaL_newstate();
+	if (L == NULL)
+		return 0;
+
+	char *buf = calloc(size + 1, sizeof(char));
+	if (buf == NULL)
+		return 0;
+	memcpy(buf, data, size);
+	buf[size] = '\0';
+
+	luaL_traceback(L, L, buf, 1);
+
+	free(buf);
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return 0;
+}

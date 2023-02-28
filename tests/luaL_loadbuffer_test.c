bb427d72e38b0fbf47a28ed9878f0ae799eb95de
Sergey Bronnikov <estetus@gmail.com>
2023-02-28 18:04:05 +0300

    tests: add luaL_loadbuffer_test

---
+++ tests/luaL_loadbuffer_test.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	lua_State *L = luaL_newstate();
+	if (L == NULL)
+		return 0;
+
+	luaL_loadbuffer(L, (const char *)data, size, "fuzz");
+	lua_pcall(L, 0, 0, 0);
+
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return 0;
+}

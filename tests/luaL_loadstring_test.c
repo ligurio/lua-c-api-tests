087580682776218ea2ff6962e8bb5e98e2750e34
Sergey Bronnikov <estetus@gmail.com>
2023-02-28 18:04:21 +0300

    tests: add luaL_loadstring_test

---
+++ tests/luaL_loadstring_test.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	lua_State *L = luaL_newstate();
+	if (L == NULL)
+		return 0;
+
+	luaL_openlibs(L);
+
+	size_t str_len = size + 1;
+	char *str = calloc(str_len, sizeof(char));
+	if (str == NULL)
+		return 0;
+	memcpy(str, data, size);
+	str[size] = '\0';
+
+	if (luaL_loadstring(L, str) != LUA_OK) {
+		return 0;
+	}
+
+	lua_pcall(L, 0, 0, 0);
+
+	free(str);
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return 0;
+}

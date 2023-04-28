3975b64c2ebd90ed9d94bd185bd9aa5943bd2b1d
Sergey Bronnikov <estetus@gmail.com>
2023-04-28 21:01:58 +0300

    tests: replace constants with random values

---
+++ tests/luaL_addgsub_test.cc
@@ -0,0 +1,53 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
+
+extern "C" int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	FuzzedDataProvider fdp(data, size);
+
+	lua_State *L = luaL_newstate();
+	if (L == NULL)
+		return 0;
+
+	luaL_Buffer buf;
+	size_t buf_size = fdp.ConsumeIntegralInRange<int8_t>(0, INT8_MAX);
+	char *s = luaL_buffinitsize(L, &buf, buf_size);
+	auto str = fdp.ConsumeRandomLengthString(buf_size);
+	memcpy(s, str.c_str(), buf_size);
+	luaL_pushresultsize(&buf, buf_size);
+
+	size_t max_length = fdp.ConsumeIntegralInRange<size_t>(1, INT8_MAX);
+	auto str1 = fdp.ConsumeRandomLengthString(max_length);
+	auto str2 = fdp.ConsumeRandomLengthString(max_length);
+	auto str3 = fdp.ConsumeRandomLengthString(max_length);
+	const char *c_str1 = str1.c_str();
+	const char *c_str2 = str2.c_str();
+	const char *c_str3 = str3.c_str();
+	if (strlen(c_str1) == 0 ||
+	    strlen(c_str2) == 0 ||
+	    strlen(c_str3) == 0)
+		return -1;
+	luaL_addgsub(&buf, c_str1, c_str2, c_str3);
+
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return 0;
+}

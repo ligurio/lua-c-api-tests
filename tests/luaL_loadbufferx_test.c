1cf5c11c7a65e3c9dedcaf1ed16e38e494a01f66
Sergey Bronnikov <estetus@gmail.com>
2023-09-13 19:15:36 +0300

    tests: update luaL_loadbufferx_test

--- tests/luaL_loadbufferx_test.c
+++ tests/luaL_loadbufferx_test.c
@@ -1,34 +1,43 @@
 #include <stdint.h>
-#include <stddef.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdlib.h> /* malloc, free */
+#include <string.h> /* memcpy */
 
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
 
+/*
+ * The main purpose of the test is testing Lua frontend (lexer, parser).
+ * The test doesn't execute a loaded chunk to be quite fast.
+ */
 
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
 	lua_State *L = luaL_newstate();
 	if (L == NULL)
 		return 0;
 
-	luaL_openlibs(L);
-
+	/*
+	 * The string "mode" controls whether the chunk can be text or binary
+	 * (that is, a precompiled chunk). It may be the string "b" (only binary
+	 * chunks), "t" (only text chunks), or "bt" (both binary and text). The
+	 * default is "bt".
+	 * Lua runtime (at least PUC Rio Lua and LuaJIT) has bytecode and Lua
+	 * parsers. It is desired to test both parsers, however, in LuaJIT
+	 * bytecode parser failed with assertion:
+	 *
+	 * LuaJIT ASSERT lj_bcread.c:123: bcread_byte: buffer read overflow
+	 *
+	 * so in LuaJIT only text mode is used and therefore only text parser is
+	 * tested.
+	 */
 #ifdef LUAJIT
-	/* See https://luajit.org/running.html. */
-	luaL_dostring(L, "jit.opt.start('hotloop=1')");
-	luaL_dostring(L, "jit.opt.start('hotexit=1')");
-	luaL_dostring(L, "jit.opt.start('recunroll=1')");
-	luaL_dostring(L, "jit.opt.start('callunroll=1')");
-#endif /* LUAJIT */
-
 	const char *mode = "t";
-	int res = luaL_loadbufferx(L, (const char *)data, size, "fuzz", mode);
-	if (res == LUA_OK) {
-		lua_pcall(L, 0, 0, 0);
-	}
+#else
+	const char *mode = "bt";
+#endif /* LUAJIT */
+	luaL_loadbufferx(L, (const char *)data, size, "fuzz", mode);
 
 	lua_settop(L, 0);
 	lua_close(L);

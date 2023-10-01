0841c133342e9d73c3efc7ebb3ff5d4a1064756b
Sergey Bronnikov <estetus@gmail.com>
2023-10-01 21:01:34 +0300

    tests: update lua_load_test
    Previously `lua_load_test` generated a buffer outside of `Reader`
    function and passed it as a file descriptor created using `fmemopen()`.
    Then `Reader()` read 1 byte per call from this file descriptor an
    returned to `lua_load`. This approach works fine, however, it is better
    to generate buffers and their sizes inside a `Reader` function because
    this way test works faster. The patch implements this by passing
    `FuzzingDataProvider` object to a `Reader`.
    
    Speed before the patch: ~2200 exec/s.
    Speed after the patch: ~2900 exec/s.

---
+++ tests/lua_load_test.cc
@@ -0,0 +1,71 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+typedef struct {
+	FuzzedDataProvider *fdp;
+} dt;
+
+static const char *
+Reader(lua_State *L, void *data, size_t *size)
+{
+	dt *test_data = (dt *)data;
+	static char *buf = NULL;
+
+	FuzzedDataProvider *fdp = test_data->fdp;
+	uint8_t max_str_size = fdp->ConsumeIntegral<uint8_t>();
+	if (fdp->remaining_bytes() < max_str_size)
+		return NULL;
+	auto str = fdp->ConsumeRandomLengthString(max_str_size);
+	*size = str.size();
+
+	free(buf);
+	buf = (char *)malloc(*size);
+	assert(buf);
+	memcpy(buf, str.c_str(), *size);
+
+	return buf;
+}
+
+extern "C" int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	lua_State *L = luaL_newstate();
+	if (L == NULL)
+		return 0;
+
+	luaL_openlibs(L);
+
+	FuzzedDataProvider fdp(data, size);
+	dt test_data;
+	test_data.fdp = &fdp;
+
+	const char *mode = "t";
+	int res = lua_load(L, Reader, &test_data, "libFuzzer", mode);
+	if (res == LUA_OK) {
+		lua_pcall(L, 0, 0, 0);
+	}
+
+	lua_settop(L, 0);
+	lua_close(L);
+
+	return 0;
+}

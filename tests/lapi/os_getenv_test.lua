ba5c59f13e3fefcf096df809b1b599fabe08a422
Sergey Bronnikov <estetus@gmail.com>
2025-01-16 10:55:54 +0300

    tests/lapi: os tests
    The patch adds tests for the functions in os library except the
    following functions: `os.tmpname`, `os.rename`, `os.remove`,
    `os.execute`, `os.clock`.

---
+++ tests/lapi/os_getenv_test.lua
@@ -0,0 +1,21 @@
+--[[
+SPDX-License-Identifier: ISC
+Copyright (c) 2023-2025, Sergey Bronnikov.
+
+6.9 – Operating System Facilities
+https://www.lua.org/manual/5.3/manual.html#6.9
+
+Synopsis: os.getenv(varname)
+]]
+
+
+local luzer = require("luzer")
+
+local function TestOneInput(buf)
+    os.getenv(buf)
+end
+
+local args = {
+    artifact_prefix = "os_getenv_",
+}
+luzer.Fuzz(TestOneInput, nil, args)

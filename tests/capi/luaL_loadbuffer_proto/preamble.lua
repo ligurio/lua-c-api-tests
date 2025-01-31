c410a5556d140d3b0b8d20c15e60bf6fe4057a11
Sergey Bronnikov <estetus@gmail.com>
2025-01-31 10:34:05 +0300

    tests/capi: support bitwise operations in luaL_loadbuffer_proto
    The `bit32` library has been deprecated in PUC Rio Lua 5.3 [1].
    Missed bitwise functions have been replaced by appropriate bitwise
    operations [2]. The patch adds support of bitwise operations to
    the test `luaL_loadbuffer_proto`.
    
    Bitwise operators introduced in Lua 5.3 breaks parsing in Lua 5.1
    and 5.2, to avoid this metamethods in preamble.lua uses a function
    `bitwise_op`. It was implemented for bitwise tests added in the
    commit d4b91f6a ("tests/lapi: add bitop tests").
    
    1. https://www.lua.org/manual/5.3/manual.html#8.2
    2. https://www.lua.org/manual/5.3/manual.html#3.4.2

--- tests/capi/luaL_loadbuffer_proto/preamble.lua
+++ tests/capi/luaL_loadbuffer_proto/preamble.lua
@@ -8,6 +8,26 @@ local not_nan_and_nil = function(val)
     return (val ~= val or val == nil) and DEFAULT_NUMBER or val
 end
 
+local function bitwise_op(op_name)
+    return function(...)
+        local n = select('#', ...)
+        local chunk
+        -- Bitwise exclusive OR and bitwise NOT have the same
+        -- operator.
+        if (op_name == '&' or op_name == '|') then
+            assert(n > 1)
+        end
+        if n == 1 then
+            local x = ...
+            chunk = ('return %s %d'):format(op_name, x)
+        else
+            local op_name_ws = (' %s '):format(op_name)
+            chunk = 'return ' .. table.concat({...}, op_name_ws)
+        end
+        return assert(load(chunk))()
+    end
+end
+
 local __add = function(v1, v2)
     return always_number(v1) + always_number(v2)
 end
@@ -76,6 +96,30 @@ local __idiv = function(v1, v2)
     local chunk = ('%d // %d'):format(always_number(v1), always_number(v2))
     return assert(loadstring(chunk))()
 end
+local __band = function(v1, v2)
+    local band = bitwise_op('&')
+    return band(always_number(v1), always_number(v2))
+end
+local __bor = function(v1, v2)
+    local bor = bitwise_op('|')
+    return bor(always_number(v1), always_number(v2))
+end
+local __bxor = function(v1, v2)
+    local bxor = bitwise_op('~')
+    return bxor(always_number(v1), always_number(v2))
+end
+local __bnot = function(v)
+    local bnot = bitwise_op('~')
+    return bnot(always_number(v))
+end
+local __shl = function(v1, v2)
+    local shl = bitwise_op('<<')
+    return shl(always_number(v1), always_number(v2))
+end
+local __shr = function(v1, v2)
+    local shr = bitwise_op('>>')
+    return shr(always_number(v1), always_number(v2))
+end
 
 debug.setmetatable('string', {
     __add = __add,
@@ -89,6 +133,12 @@ debug.setmetatable('string', {
     __pow = __pow,
     __sub = __sub,
     __unm = __unm,
+    __band = __band,
+    __bor= __bor,
+    __bxor = __bxor,
+    __bnot = __bnot,
+    __shl = __shl,
+    __shr = __shr,
 })
 debug.setmetatable(0, {
     __add = __add,

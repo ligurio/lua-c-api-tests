1afca942f73d4ebb69370ac009a50b5673eefb86
Sergey Bronnikov <estetus@gmail.com>
2025-07-28 18:19:58 +0300

    tests/lapi: fix string_byte_test
    The patch fixes the following issues:
    
    - `string.byte()` returns the internal numeric codes of the
      characters, not a single number.
    - `string.char()` returns `nil` when values `i` or `j` are
       outside the acceptable range (less than zero and greater than
       the length of the string). It is not documented.
    
    Follows up #124

--- tests/lapi/string_byte_test.lua
+++ tests/lapi/string_byte_test.lua
@@ -1,4 +1,4 @@
---[[
+--[=[
 SPDX-License-Identifier: ISC
 Copyright (c) 2023-2025, Sergey Bronnikov.
 
@@ -7,9 +7,9 @@ https://www.lua.org/manual/5.3/manual.html#6.4
 
 string.byte gets confused with some out-of-range negative indices,
 https://www.lua.org/bugs.html#5.1.3-9
-]]
 
--- Synopsis: string.byte(s [, i [, j]])
+Synopsis: string.byte(s [, i [, j]])
+]=]
 
 local luzer = require("luzer")
 local test_lib = require("lib")
@@ -18,17 +18,15 @@ local function TestOneInput(buf, _size)
     local fdp = luzer.FuzzedDataProvider(buf)
     os.setlocale(test_lib.random_locale(fdp), "all")
     local str = fdp:consume_string(test_lib.MAX_STR_LEN)
-    local i = fdp:consume_integer(0, test_lib.MAX_INT)
-    local j = fdp:consume_integer(0, test_lib.MAX_INT)
+    local i = fdp:consume_integer(test_lib.MIN_INT, test_lib.MAX_INT)
+    local j = fdp:consume_integer(test_lib.MIN_INT, test_lib.MAX_INT)
     -- `string.byte()` is the same as `str:byte()`.
     assert(string.byte(str, i, j) == str:byte(i, j))
-    local char_code = string.byte(str, i, j)
-    if char_code then
-        assert(type(char_code) == "number")
-        local byte = string.char(char_code)
-        assert(byte)
-        assert(byte == str)
-    end
+    -- Note, `string.byte()` returns `nil` when values `i` or `j`
+    -- are outside the acceptable range (less than zero and
+    -- greater than the length of the string). It is undocumented.
+    local bytes = string.char(string.byte(str, i, j))
+    assert(bytes == string.sub(str, i, j))
 end
 
 local args = {

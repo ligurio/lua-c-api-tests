ef021a8f93b16b300d7a871cfc648de4224ce893
Sergey Bronnikov <estetus@gmail.com>
2025-07-28 18:20:09 +0300

    tests/lapi: fix string_rep_test
    The patch fixes description (synopsis is at the end of the
    description) and fixes lower boundary for `n`, with `n` equal
    to zero assertion calculates negative number in right part of
    expression.
    
    Follows up #124

--- tests/lapi/string_rep_test.lua
+++ tests/lapi/string_rep_test.lua
@@ -5,10 +5,10 @@ Copyright (c) 2023-2025, Sergey Bronnikov.
 6.4 – String Manipulation
 https://www.lua.org/manual/5.3/manual.html#6.4
 
-Synopsis: string.rep(s, n [, sep])
-
 read overflow in 'l_strcmp',
 https://github.com/lua/lua/commit/f623b969325be736297bc1dff48e763c08778243
+
+Synopsis: string.rep(s, n [, sep])
 ]]
 
 local luzer = require("luzer")
@@ -18,7 +18,7 @@ local function TestOneInput(buf, _size)
     local fdp = luzer.FuzzedDataProvider(buf)
     os.setlocale(test_lib.random_locale(fdp), "all")
     -- Huge length leads to slow units.
-    local n = fdp:consume_integer(0, test_lib.MAX_STR_LEN)
+    local n = fdp:consume_integer(1, test_lib.MAX_STR_LEN)
     local s = fdp:consume_string(test_lib.MAX_STR_LEN)
     local sep = fdp:consume_string(test_lib.MAX_STR_LEN)
     local len = string.len(string.rep(s, n, sep))
